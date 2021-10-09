517dd090b11be44bc28c258f3c7cf7e5e37c210e
Yulia-ufa <90646626+Yulia-ufa@users.noreply.github.com>
2021-10-09 20:00:42 +0300

    feature:Added menu

--- menu.dart
+++ menu.dart
@@ -15,6 +15,25 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               Person(),
+              const SizedBox(height: 20.0),
+              Division(),
+              const SizedBox(height: 30.0),
+              const Data(text: 'Персональные данные'),
+              const SizedBox(height: 20.0),
+              const Data(text: 'Настройки'),
+              const SizedBox(height: 20.0),
+              const Data(text: 'Электронное Заявление'),
+              const SizedBox(height: 10.0),
+              Division(),
+              const SizedBox(height: 30.0),
+              const Data(text: 'Реферальный Код'),
+              const SizedBox(height: 20.0),
+              const Data(text: 'Часто задаваемые вопросы'),
+              const SizedBox(height: 20.0),
+              const Data(text: 'Наш справочник'),
+              const SizedBox(height: 20.0),
+              const Data(text: 'Сообщество'),
+              const SizedBox(height: 20.0),
             ],
           ),
         ),
@@ -44,7 +63,7 @@ class Person extends StatelessWidget {
             ],
           ),
         ),
-        const SizedBox(width: 10.0),
+        const SizedBox(width: 30.0),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
@@ -60,6 +79,57 @@ class Person extends StatelessWidget {
           ],
         ),
       ],
+    );
+  }
+}
+
+class Data extends StatelessWidget {
+  const Data({this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 1,
+                    blurRadius: 1,
+                    offset: const Offset(0, 0),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(width: 10.0),
+            Text(
+              text,
+              style: const TextStyle(fontWeight: FontWeight.w300),
+            ),
+          ],
+        ),
+        const Icon(Icons.arrow_forward_ios),
+      ],
+    );
+  }
+}
+
+class Division extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1.0,
+      color: Colors.black12,
     );
   }
 }
